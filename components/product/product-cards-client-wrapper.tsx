77ffc6b28866f7490d5a4464be50b7bcad6a3480
Zach <zachariedupain@hotmail.fr>
2023-11-05 09:55:58 +0400

    feat: search-bar in its own comp

--- components/product/product-cards-client-wrapper.tsx
+++ components/product/product-cards-client-wrapper.tsx
@@ -6,7 +6,7 @@ import { Product } from '@prisma/client'
 
 import ProductCard from '@/components/product/product-card'
 
-import { AiOutlineSearch, AiOutlineClose } from 'react-icons/ai'
+import SearchBarMain from '../search-bar'
 
 interface ClientWrapperProps {
   allProducts: Product[]
@@ -23,35 +23,13 @@ const ClientWrapper: FC<ClientWrapperProps> = ({ allProducts }) => {
     ? filteredProducts
     : allProducts
 
-  const handleSearchQuery = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    const search = evt.target.value
-    setSearchQuery(search)
-  }
-
   return (
     <>
-      <div className="relative flex flex-1 justify-center items-center w-full 3xl:justify-center !h-fit">
-        <div className="z-50 absolute left-[2%] flex justify-center items-center">
-          {searchQuery ? (
-            <AiOutlineClose
-              className="align-middle text-[#99A1B3] shrink-0 w-[18px]"
-              onClick={() => setSearchQuery('')}
-            />
-          ) : (
-            <AiOutlineSearch
-              className={`align-middle text-[#99A1B3] shrink-0 w-[18px]`}
-            />
-          )}
-        </div>
-        <input
-          id="mySearchInput"
-          type="search"
-          onChange={handleSearchQuery}
-          value={searchQuery}
-          placeholder={'Start typing a product name'}
-          className="p-2 pl-9 flex text-sm w-[100%] relative pr-1 py-1 h-10 outline-none focus:outline-orange-500 items-center text-left bg-gray-100"
-        />
-      </div>
+      <SearchBarMain
+        searchQuery={searchQuery}
+        onSearch={(query) => setSearchQuery(query)}
+      />
+
       <div className="flex flex-wrap gap-x-1 gap-y-2 justify-between items-center">
         {!productsToDisplay.length ? (
           <span className="px-5 mx-auto text-center">
---
+++ components/search-bar.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react'
+
+import { AiOutlineSearch, AiOutlineClose } from 'react-icons/ai'
+
+interface SearchBarMainProps {
+  onSearch: (search: string) => void
+  searchQuery: string
+}
+
+const SearchBarMain: FC<SearchBarMainProps> = ({ onSearch, searchQuery }) => {
+  return (
+    <div className="relative flex flex-1 justify-center items-center w-full 3xl:justify-center !h-fit">
+      <div className="z-50 absolute left-[2%] flex justify-center items-center">
+        {searchQuery ? (
+          <AiOutlineClose
+            className="align-middle text-[#99A1B3] shrink-0 w-[18px]"
+            onClick={() => onSearch('')}
+          />
+        ) : (
+          <AiOutlineSearch
+            className={`align-middle text-[#99A1B3] shrink-0 w-[18px]`}
+          />
+        )}
+      </div>
+      <input
+        id="mySearchInput"
+        type="search"
+        onChange={(evt) => onSearch(evt.target.value)}
+        value={searchQuery}
+        placeholder={'Start typing a product name'}
+        className="p-2 pl-9 flex text-sm w-[100%] relative pr-1 py-1 h-10 outline-none focus:outline-orange-500 items-center text-left bg-gray-100"
+      />
+    </div>
+  )
+}
+
+export default SearchBarMain
